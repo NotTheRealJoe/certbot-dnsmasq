d123eb249aca2ac0c8b33d4d500c2d9358a68924
NotTheRealJoe <32490063+NotTheRealJoe@users.noreply.github.com>
2019-09-09 15:44:19 +0000

    Remove old named file

--- certbot-auto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-domainString=""
-for d in $@; do
-	domainString="$domainString -d $d"
-done
-certbot certonly $domainString --agree-tos --manual --manual-public-ip-logging-ok --preferred-challenges=dns --manual-auth-hook ./certbot-manual-authenticator.sh --manual-cleanup-hook ./certbot-manual-cleanup.sh
